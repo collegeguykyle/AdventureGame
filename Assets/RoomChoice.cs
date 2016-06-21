40c7640f147def1a85ffbb5918fce8529fc59eee
Kyle R Hopkins <Kyle R Hopkins>
2016-06-20 22:00:23 -0500

    Moving selected characters to chosen tasks.  (Incomplete)

--- Assets/RoomChoice.cs
+++ Assets/RoomChoice.cs
@@ -5,20 +5,20 @@ using System.Collections;
 public class RoomChoice : MonoBehaviour {
 
     bool charsOnScreen = false;
-    public Character.Hero hero = Character.Hero.None;
 
-    // Use this for initialization
+
 	void Start () {
 
-        Vector2 blah = new Vector2(0, 0);
-        Draw.line("test Line", blah, blah, true);
+        Vector2 blah = new Vector2(0, 0);  //testing
+        Draw.line("test Line", blah, blah, true); //testing
     }
 	
-	// Update is called once per frame
+
 	void Update () {
         //Bring characters onto screen at start of room
         if (!charsOnScreen) charsOnScreen = Movement.enterRoom(3f);
         
-
 	}
+
+
 }
---
+++ Assets/RoomOption.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomOption : MonoBehaviour {
+
+    public enum Type { explore, search, task, group, camp, none}
+    public Type type = Type.none;
+    public GameObject GameControl;
+    public GameObject who;
+
+
+    void Start()
+    {
+        GameControl = GameObject.Find("EventSystem");
+    }
+
+    void Update ()
+    {
+        if (who) Movement.moveUnit(who, gameObject.transform.position, 2f);
+    }
+
+	public void onclick()
+    {
+        Debug.Log("blaaaah");
+        who = GameControl.GetComponent<Unit_Select>().selected;
+        print(who);
+    }
+
+}
